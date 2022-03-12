29a6685959b99efac648009ce86b2eb35b77ea41
hugosenari <hugosenari@gmail.com>
2022-03-12 04:35:09 -0300

    chore: re index notetxt

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A library that implements the note.txt specification for note taking.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-notetxt-master.flake = false;
+  inputs.src-notetxt-master.owner = "mrshu";
+  inputs.src-notetxt-master.ref   = "refs/heads/master";
+  inputs.src-notetxt-master.repo  = "nim-notetxt";
+  inputs.src-notetxt-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-notetxt-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-notetxt-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
